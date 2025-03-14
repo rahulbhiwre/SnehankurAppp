c3687a30bc36f89637a63b5e7fbfd2a15e04c536
Rahul Bhiwre <51237167+rahulbhiwre@users.noreply.github.com>
2025-03-14 17:40:08 +0530

    edited main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import { createRoot } from "react-dom/client";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")!).render(<App />);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
 import App from "./App";
-import "./index.css";
 
-import { HashRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <App />
   </HashRouter>
