5b9e234d03b9d2b1a0636f46b3ffbcfec026177b
Rahul Bhiwre <51237167+rahulbhiwre@users.noreply.github.com>
2025-03-23 15:15:12 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,5 @@ export default defineConfig({
     port: 8081,
     open: true,
   },
-  base: "/", // Replace with your repository name
+  base: "/snehankurr", // Replace with your repository name
 });
